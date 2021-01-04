9b0f13e139577972404eb8c6e3628af75dafe1fd
kubota <polykkpodh1243@gmail.com>
2021-01-04 20:56:12 +0900

    Revert "add header"
    This reverts commit 0438b561e45b2e4dcbf23ab969c94504ef7648ff.

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,41 +1,15 @@
 import * as React from 'react'
-import {
-  Link,
-  useHistory,
-} from 'react-router-dom'
-import styled from 'styled-components'
-import { Header } from '../components/header'
-
-const HeaderArea = styled.div`
-  position: fixed;
-  right: 0;
-  top: 0;
-  left: 0;
-`
-
-const Wrapper = styled.div`
-  bottom: 0;
-  left: 0;
-  position: fixed;
-  right: 0;
-  top: 3rem;
-  padding: 0 1rem;
-`
+import { useHistory } from 'react-router-dom'
+import { Button } from '../components/button'
 
 export const History: React.FC = () => {
-
+  const history = useHistory()
   return (
     <>
-      <HeaderArea>
-        <Header title="履歴">
-          <Link to="/editor">
-            エディタに戻る
-          </Link>
-        </Header>
-      </HeaderArea>
-      <Wrapper>
-        TODO: 履歴表示
-      </Wrapper>
+      <h1>History</h1>
+      <Button onClick={() => history.push('/editor')}>
+        エディタへ戻る
+      </Button>
     </>
   )
 }
